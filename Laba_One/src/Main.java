b4e308a7282dc011e42d6eeed18aa54ab6d79d4a
Roman <lerner.r@mail.ru>
2022-11-22 23:43:06 +0300

    Update Main.java

--- Laba_One/src/Main.java
+++ Laba_One/src/Main.java
@@ -9,15 +9,9 @@ public class Main {
     public static final String COMMA_DELIMITER = ",", DASH_DELIMITER = "-";
     public static int lineNumber = 0;
     public static double resultOfEET, celsiusTemperature, windSpeedMiles, relativeHumidity, fahrenheitTemperature, windSpeedMS;
-    public static List<Integer> day = new ArrayList<>();
-    public static List<Integer> month = new ArrayList<>();
-    public static List<Integer> year = new ArrayList<>();
-    public static List<Integer> monthComfCount = new ArrayList<>();
-    public static List<Integer> monthSubComfCount = new ArrayList<>();
-    public static List<Integer> monthUnComfCount = new ArrayList<>();
-    public static List<Boolean> IsComf = new ArrayList<>();
-    public static List<Boolean> IsSubComf = new ArrayList<>();
-    public static List<Boolean> IsUnComf = new ArrayList<>();
+    public static List<Integer> day = new ArrayList<>(), month = new ArrayList<>(), year = new ArrayList<>();
+    public static List<Integer> monthComfCount = new ArrayList<>(), monthSubComfCount = new ArrayList<>(), monthUnComfCount = new ArrayList<>();
+    public static List<Boolean> IsComf = new ArrayList<>(), IsSubComf = new ArrayList<>(), IsUnComf = new ArrayList<>();
     public static List<Double> ET = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -27,9 +21,6 @@ public class Main {
         Reader();
         max = monthComfCount.get(0);
         for(int i = 0; i < monthComfCount.size(); i++){
-            //System.out.print(monthComfCount.get(i));
-            //System.out.print(" ");
-            //System.out.println(i);
             if(monthComfCount.get(i) > max){
                 max = monthComfCount.get(i);
                 monthNumber = i;
@@ -67,7 +58,6 @@ public class Main {
         }
     }
 
-
     public static void Reader() {
         List<List<String>> records = new ArrayList<>();
         int comfCounter = 0, monthCounter = 0, subComfCounter = 0, unComfCounter = 0;
@@ -80,7 +70,6 @@ public class Main {
                 lineNumber++;
                 records.add(getRecordFromLine(scanner.nextLine()));
                 ET.add(resultOfEET);
-                //System.out.println(ET.get(ET.size() - 1));
                 getDateFromLine(scannerTwo.nextLine());
                 if(ET.get(ET.size() - 1) > 12 && ET.get(ET.size() - 1) < 24){
                     IsComf.add(true);
@@ -104,7 +93,6 @@ public class Main {
                     if(IsComf.get(IsComf.size() - 1)){
                         comfCounter++;
                         monthComfCount.set(monthCounter, comfCounter);
-                        //System.out.println(monthComfCount.get(monthCounter - 1));
                     }
                     if(IsSubComf.get(IsComf.size() - 1)){
                         subComfCounter++;
@@ -119,7 +107,6 @@ public class Main {
                     if(IsComf.get(IsComf.size() - 1)){
                         comfCounter++;
                         monthComfCount.set(monthCounter - 1, comfCounter);
-                        //System.out.println(monthComfCount.get(monthCounter - 1));
                     }
                     if(IsSubComf.get(IsComf.size() - 1)){
                         subComfCounter++;
@@ -141,7 +128,6 @@ public class Main {
                     if(IsComf.get(IsComf.size() - 1)){
                         comfCounter++;
                         monthComfCount.set(monthCounter - 1, comfCounter);
-                        //System.out.println(monthComfCount.get(monthCounter - 1));
                     }
                     if(IsSubComf.get(IsComf.size() - 1)){
                         subComfCounter++;
@@ -189,16 +175,10 @@ public class Main {
 
             }
         }
-        //System.out.println(values);
-
         if(fahrenheitTemperature != -1000 && relativeHumidity != -1000 && windSpeedMiles != -1000) {
             celsiusTemperature = (fahrenheitTemperature - 32) * 5 / 9;
             windSpeedMS = windSpeedMiles * 0.44704;
-            resultOfEET = 37 - ((37 - celsiusTemperature) / (0.68 - 0.0014 * relativeHumidity) + (1 / (1.76 + 1.4 * Math.pow(windSpeedMS, 0.75)))) - 0.29 * celsiusTemperature * (1 - (relativeHumidity / 100));
-
-            //System.out.print(resultOfEET);
-            //System.out.print(" ");
-            //System.out.println(lineNumber);
+            resultOfEET = 37 - (37 - celsiusTemperature) / (0.68 - 0.0014 * relativeHumidity + 1 / (1.76 + 1.4 * Math.pow(windSpeedMS, 0.75))) - 0.29 * celsiusTemperature * (1 - relativeHumidity/100);
         }
         fahrenheitTemperature = -1000;
         relativeHumidity = -1000;
@@ -221,16 +201,12 @@ public class Main {
                 if (indexD == 0)
                     try {
                         year.add(Integer.parseInt(data));
-                        //System.out.print(data);
-                        //System.out.print(" ");
                     } catch (NumberFormatException e) {
                         System.out.println("Incorrect data in " + lineNumber + " line");
                     }
                 else if (indexD == 1) {
                     try {
                         month.add(Integer.parseInt(data));
-                        //System.out.print(month.get(month.size() - 1));
-                        //System.out.print(" ");
                     } catch (NumberFormatException e) {
                         System.out.println("Incorrect data in " + lineNumber + " line");
                     }
@@ -238,7 +214,6 @@ public class Main {
                 else if (indexD == 2) {
                     try {
                         day.add(Integer.parseInt(data) * -1);
-                        //System.out.println(day.get(day.size() - 1));
                     } catch (NumberFormatException e) {
                         System.out.println("Incorrect data in " + lineNumber + " line");
                     }
@@ -248,14 +223,6 @@ public class Main {
 
             }
         }
-        //System.out.println(dates);
-
-        //System.out.print(day);
-        //System.out.print(" ");
-        //System.out.print(month);
-        //System.out.print(" ");
-        //System.out.println(year);
         return dates;
     }
 }
-
